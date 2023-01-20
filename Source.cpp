7445c84f7b560bc9d4f6512ed4249173a0bd726f
TheZionKnight <115610198+TheZionKnight@users.noreply.github.com>
2023-01-20 21:11:51 +0200

    Added mark command
    And some win/lose functionality

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,7 @@
 #include <iomanip> //for setw
 #include <stdlib.h> //for system("cls")
 #include <cstdlib> //for rand()
-#include <time.h>
+#include <time.h> //for rand()
 
 void showField(char** field, int size);
 
@@ -14,12 +14,22 @@ void command(char** realField, char** revealed, int size, int bombs);
 
 void addOne(int i, int j, char** field, int size);
 
-void fillField(char** field, int size, char symbol) {
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			field[i][j] = symbol;
-		}
-	}
+void fillField(char** field, int size, char symbol);
+
+void Engine();
+
+int whichAction();
+
+void open(int i, int j, char** realField, char** revealed, int size, bool& lose, bool& win, int& revealedOrMarkedCorrectly);
+
+void mark(int i, int j, char** realField, char** revealed, int size, bool& win, int& revealedOrMarkedCorrectly);
+
+void unmark(int i, int j, char** realField, char** revealed, int size, int& revealedOrMarkedCorrectly);
+
+int main() {
+
+	Engine();
+
 }
 
 void Engine() {
@@ -40,17 +50,82 @@ void Engine() {
 	fillField(realField, size, ' ');
 	fillField(revealed, size, '?');
 	fillBombs(realField, size, bombs);
+	showField(revealed, size);
 	command(realField, revealed, size, bombs);
 }
 
-int main() {
-
-	Engine();
-
+int whichAction() {
+	char input[10];
+	std::cin >> input;
+	if (input[0] == 'o' && input[1] == 'p' && input[2] == 'e' && input[3] == 'n' && input[4]=='\0') {
+		return 1;
+	}
+	if (input[0] == 'm' && input[1] == 'a' && input[2] == 'r' && input[3] == 'k' && input[4] == '\0') {
+		return 2;
+	}
+	if (input[0] == 'u' && input[1] == 'n' && input[2] == 'm' && input[3] == 'a' && input[4] == 'r' && input[5] == 'k' && input[6] == '\0') {
+		return 3;
+	}
+	else {
+		std::cout << "Wrong input. Try again: "<<std::endl;
+		return whichAction();
+	}
 }
 
 void command(char** realField, char** revealed, int size, int bombs) {
-	showField(realField, size);
+	bool win = false, lose = false;
+	int revealedOrMarkedCorrectly = 0;
+	while (!win && !lose) {
+		int action = whichAction(); //open-1, mark-2, unmark-3
+		int i, j;
+		std::cin >> j >> i;
+		switch (action) {
+		case 1:
+			open(i, j, realField, revealed, size, lose, win, revealedOrMarkedCorrectly);
+			break;
+		case 2:
+			mark(i, j, realField, revealed, size, win, revealedOrMarkedCorrectly);
+			break;
+		case 3:
+			unmark(i, j, realField, revealed, size, revealedOrMarkedCorrectly);
+			break;
+		}
+	}
+	if (win) {
+		std::cout << std::endl << "Congratulations! You won!";
+		return;
+	}
+	if (lose) {
+		std::cout << std::endl << "Uh-oh! You stepped on a mine!";
+		return;
+	}
+}
+
+void open(int i, int j, char** realField, char** revealed, int size, bool& lose, bool& win, int& revealedOrMarkedCorrectly) {
+
+}
+
+void mark(int i, int j, char** realField, char** revealed, int size, bool& win, int& revealedOrMarkedCorrectly) {
+	revealed[i][j] = 'X';
+	if (realField[i][j] == 'X') {
+		revealedOrMarkedCorrectly++;
+		if (revealedOrMarkedCorrectly == size * size) {
+			win = true;
+		}
+	}
+	showField(revealed, size);
+}
+
+void unmark(int i, int j, char** realField, char** revealed, int size, int& revealedOrMarkedCorrectly) {
+	
+}
+
+void fillField(char** field, int size, char symbol) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			field[i][j] = symbol;
+		}
+	}
 }
 
 void showField(char** field, int size) {
